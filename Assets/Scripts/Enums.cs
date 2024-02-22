2ab9bb0e090c123dbf1b3521ea6ceff7bdb414f1
Jonathan Bowen <jonathanbowen7@gmail.com>
2024-02-22 15:57:15 -0500

    HealthBar Added
    Healthbar in top left corner that changes as health goes down.

---
+++ Assets/Scripts/Enums.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum CharacterClasses
+{
+    Paladin,
+    Mage,
+    Thief,
+    Cleric
+};
+
+public enum CharacterTypes
+{
+    Human,
+    Monster
+};
+
+public enum AbilityScoreNames
+{
+    Strength,
+    Dexterity,
+    Consitution,
+    Intelligence,
+    Wisdom,
+    Charisma
+};
+
+public enum AttackType
+{
+    Attack,
+    Heal
+};
